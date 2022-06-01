e493c93454e6c37e50bb5fe64c62d19f4f09f3a6
Agustin Glaiel <agustinglaiel@Agustins-MacBook-Pro.local>
2022-06-01 10:39:25 -0300

    ejs clase

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -223,13 +223,34 @@ function dibujarImagen (posX, posY)
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
-    console.log(posX, posY);
+    console.log(posX, posY); //esto es solo para seguir en consola, no es necesario
     var img = new Image();
     img.src="images/auto.png";
 
-    canvas.width = canvas.width;
+    canvas.width = canvas.width; //para que se dibuje un auto nuevo cada vez que cambiamos valores y no se pisen
     img.onload = function ()
     {
         ctx.drawImage(img, posX, posY);
     }
+}
+
+
+x=0;
+dx=2;
+function animarAuto()
+{
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var img = new Image();
+    canvas.width = canvas.width;
+    img.src="images/auto.png";
+    img.onload = function ()
+    {
+        ctx.drawImage(img, x, 100);
+    }
+    if (x>canvas.width)
+    {
+        x = 0;
+    }
+    x = x + dx;
 }
